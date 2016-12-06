d4c7b1ef9b82b53e8b35fff9dba17ba4cbcfdadc
Jordan Tart <jordan.tart3@gmail.com>
2016-12-06 18:25:08 +0000

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            css: {
+                files: ['assets/sass/*.scss'],
+                tasks: ['sass:dev']
+            }
+        },
+        sass: {
+            dev: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'expanded',
+                    noCache: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/sass/',
+                    src: ['**/*.scss'],
+                    dest: 'src/styles/',
+                    ext: '.css'
+                }]
+            },
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'compressed',
+                    noCache: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/sass/',
+                    src: ['**/*.scss'],
+                    dest: 'src/styles/',
+                    ext: '.css'
+                }]
+            }
+        },
+        uncss: {
+            dist: {
+                files: [
+                    { src: 'src/index.html', dest: 'src/styles/styles.css' }
+                ]
+            }
+        },
+        cssmin: {
+            dist: {
+                files: [
+                    { src: 'src/styles/styles.css', dest: 'dist/styles/styles.css' }
+                ]
+            }
+        },
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: ['**/*.{png,jpg,jpeg,gif}'],
+                    dest: 'dist/images'
+              }]
+           }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**/*.html',
+                    dest: 'dist/'
+              }]
+           }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-uncss');
+
+    grunt.registerTask('dev', ['sass:dev', 'watch']);
+    grunt.registerTask('dist', ['sass:dist', 'uncss:dist', 'cssmin:dist', 'imagemin:dist', 'htmlmin:dist']);
+    grunt.registerTask('default', ['dist']);
+};
